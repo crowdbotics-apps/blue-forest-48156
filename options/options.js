7b3216e4ce5a1df584249535761f433019809909
Crowdbotics <team@crowdbotics.com>
2024-05-29 15:56:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-forest-48156",
+  url: "https://blue-forest-48156.botics.co",
+  api: "https://blue-forest-48156.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
